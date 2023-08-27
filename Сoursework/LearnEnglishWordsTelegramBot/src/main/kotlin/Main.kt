554a4ef7d4b4b6f936c78a7ccc99c911798823f0
Aleksandr Golovko <alex-golovko@bk.ru>
2023-08-27 13:40:30 +0300

    lesson_2 ready

--- Сoursework/LearnEnglishWordsTelegramBot/src/main/kotlin/Main.kt
+++ Сoursework/LearnEnglishWordsTelegramBot/src/main/kotlin/Main.kt
@@ -4,14 +4,29 @@ import java.io.File
 
 fun main() {
     val wordFile = File("words.txt")
-//    wordFile.writeText(
-//        """
-//        |hello привет
-//        |dog собака
-//        |cat кошка
-//    """.trimMargin()
-//    )
-    wordFile.readLines().forEach {
+    val dictionary: MutableList<Word> = mutableListOf()
+    val linesFile = wordFile.readLines()
+
+    addWordToDictionary(linesFile, dictionary)
+
+    dictionary.forEach {
         println(it)
+    }
+}
+
+private fun addWordToDictionary(linesFile: List<String>, dictionary: MutableList<Word>) {
+    for (lineFile in linesFile) {
+        val line = lineFile.split("|")
+        dictionary.add(Word(line[0], line[1], line[2].toInt()))
+    }
+}
+
+data class Word(
+        private val text: String,
+        private val translate: String,
+        private val correctAnswersCount: Int = 0,
+) {
+    override fun toString(): String {
+        return "Слово: $text, Перевод: $translate, Правильных ответов: $correctAnswersCount"
     }
 }
