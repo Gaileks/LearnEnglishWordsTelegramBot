ae1cc424f160111e8da8dbbdea260ec805a37f6c
Aleksandr Golovko <alex-golovko@bk.ru>
2023-11-02 16:34:50 +0300

    lesson_8 ready

--- Сoursework/LearnEnglishWordsTelegramBot/src/main/kotlin/TelegramBotService.kt
+++ Сoursework/LearnEnglishWordsTelegramBot/src/main/kotlin/TelegramBotService.kt
@@ -3,19 +3,19 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-class TelegramBotService {
+private const val API_TELEGRAM = "https://api.telegram.org/bot"
+
+class TelegramBotService(private val client: HttpClient = HttpClient.newBuilder().build()) {
 
     fun getUpdates(botToken: String, updateId: Int): String {
-        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
-        val client: HttpClient = HttpClient.newBuilder().build()
+        val urlGetUpdates = "$API_TELEGRAM$botToken/getUpdates?offset=$updateId"
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
         return response.body()
     }
 
     fun sendMessage(botToken: String, chatId: Int, message: String): String {
-        val sendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$message"
-        val client: HttpClient = HttpClient.newBuilder().build()
+        val sendMessage = "$API_TELEGRAM$botToken/sendMessage?chat_id=$chatId&text=$message"
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(sendMessage)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
         return response.body()
